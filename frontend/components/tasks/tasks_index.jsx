8e096514a3769567d6b48c9e5f1293056973abeb
‘nathashas1’ <nathasha36@gmail.com>
2018-10-18 14:38:33 -0700

    new changes

---
+++ frontend/components/tasks/tasks_index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+class TasksIndex extends React.Component {
+  componentDidMount() {
+   this.props.fetchTasks();
+   window.scrollTo(0,0);
+   }
+
+   render() {
+     const tasks = this.props.tasks.map(task => {
+      return (
+        <TaskIndexItem
+          key={task.id}
+          task={task} />
+      );
+    });
+
+
+     return(
+       <div className="fullTasksPage">
+
+
+      <ul className="alltasks">
+
+          {tasks}
+
+        </ul>
+      </div>
+  );
+  }
+
+}
+
+
+export default TasksIndex;
