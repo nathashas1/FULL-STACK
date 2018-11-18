7cb9c8f58be3f25d5503973f21ecc509a8f582d3
‘nathashas1’ <nathasha36@gmail.com>
2018-11-18 12:10:32 -0800

    style

--- frontend/ask_rabbit.jsx
+++ frontend/ask_rabbit.jsx
@@ -1,10 +1,5 @@
-
-
-
-//React
 import React from 'react';
 import ReactDOM from 'react-dom';
-//Components
 import Root from './components/root';
 import configureStore from './store/store';
 import {login} from './actions/session_actions';
@@ -19,15 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
         users: { [window.currentUser.id]: window.currentUser }
       }
     };
+  
     store = configureStore(preloadedState);
     delete window.currentUser;
   } else {
     store = configureStore();
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
-  // window.login = login;
   }
-  
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
