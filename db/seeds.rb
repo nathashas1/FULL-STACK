55c0d80d66d5428d9ca6103cd9ff6a9c46854d33
‘nathashas1’ <nathasha36@gmail.com>
2018-11-19 14:00:37 -0800

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,15 +39,15 @@ Taskcategory.create!(
 User.create!(
   username: 'guest',
   password: 'password',
-  firstname: 'guest',
-  lastname: 'guest',
+  firstname: 'Gin',
+  lastname: 'Sin',
   zipcode: 94105
 )
 
 User.create!(
   username: 'guest2',
   password: 'password',
-  firstname: 'Spider',
+  firstname: 'Han',
   lastname: 'Man',
   zipcode: 94105
 )
@@ -55,15 +55,15 @@ User.create!(
 User.create!(
   username: 'guest3',
   password: 'password',
-  firstname: 'Super',
-  lastname: 'Man',
+  firstname: 'Din',
+  lastname: 'Joy',
   zipcode: 94105
 )
 
 User.create!(
   username: 'guest4',
   password: 'password',
-  firstname: 'Bat',
+  firstname: 'Mik',
   lastname: 'Man',
   zipcode: 94105
 )
@@ -128,7 +128,191 @@ User.create!(
   username: 'guest12',
   password: 'password',
   firstname: 'Sam',
-  lastname: 'Mathew',
+  lastname: 'Sim',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest13',
+  password: 'password',
+  firstname: 'Tuji',
+  lastname: 'Tuji',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Luy',
+  lastname: 'Luy',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Sij',
+  lastname: 'Saj',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Sri',
+  lastname: 'Sri',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Nat',
+  lastname: 'Mat',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Nit',
+  lastname: 'Nat',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Sob',
+  lastname: 'Sib',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Rug',
+  lastname: 'Rag',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Sre',
+  lastname: 'Rug',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Rek',
+  lastname: 'Rok',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Raj',
+  lastname: 'Rej',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Bab',
+  lastname: 'Baeb',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Ein',
+  lastname: 'Pog',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Wik',
+  lastname: 'Wiki',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Qor',
+  lastname: 'Sim',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Sik',
+  lastname: 'Tik',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Kol',
+  lastname: 'Sim',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Sam',
+  lastname: 'Jok',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Jig',
+  lastname: 'Til',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Wof',
+  lastname: 'Man',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Dok',
+  lastname: 'Jok',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Dov',
+  lastname: 'Dove',
+  zipcode: 94105
+)
+
+User.create!(
+  username: 'guest12',
+  password: 'password',
+  firstname: 'Rab',
+  lastname: 'Din',
   zipcode: 94105
 )
 
@@ -216,6 +400,7 @@ Tasker.create!(
 Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{15}.jpg"),filename:"15.jpg")
 
 
+
 Tasker.create!(
   task_category: 'Mounting and Installation',
   title: "Im a mounting helper",
@@ -284,3 +469,263 @@ Tasker.create!(
   user: User.all[10]
 )
  Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{21}.jpg"),filename:"21.jpg")
+
+ Tasker.create!(
+   task_category: 'Heavy Lifting',
+   contact: "tasker11@gmail.com",
+   title: "Im a lifting helper",
+   hourly_rate: 39,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 lifting tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional lifting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[11]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab1}.jpg"),filename:"rab1.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Heavy Lifting',
+   contact: "tasker12@gmail.com",
+   title: "Im a lifting helper",
+   hourly_rate: 25,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 lifting tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[12]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab2}.png"),filename:"rab2.png")
+
+
+
+ Tasker.create!(
+   task_category: 'Heavy Lifting',
+   contact: "tasker13@gmail.com",
+   title: "Im a lifting helper",
+   hourly_rate: 23,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 lifting tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[13]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab3}.jpg"),filename:"rab3.jpg")
+
+ Tasker.create!(
+   task_category: 'General Handyman',
+   contact: "tasker14@gmail.com",
+   title: "Im a handyman helper",
+   hourly_rate: 36,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 handyman tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[14]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab4}.jpg"),filename:"rab4.jpg")
+
+ Tasker.create!(
+   task_category: 'General Handyman',
+   contact: "tasker15@gmail.com",
+   title: "Im a handyman helper",
+   hourly_rate: 34,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 handyman tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[15]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab5}.jpeg"),filename:"rab5.jpeg")
+
+ Tasker.create!(
+   task_category: 'General Handyman',
+   contact: "tasker16@gmail.com",
+   title: "Im a handyman helper",
+   hourly_rate: 46,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 handyman tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[16]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab6}.jpg"),filename:"rab6.jpg")
+
+ Tasker.create!(
+   task_category: 'Home Improvement',
+   contact: "tasker17@gmail.com",
+   title: "Im a home improvement helper",
+   hourly_rate: 30,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 home improvement tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[17]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab7}.png"),filename:"rab7.png")
+
+
+ Tasker.create!(
+   task_category: 'Home Improvement',
+   contact: "tasker18@gmail.com",
+   title: "Im a home improvement helper",
+   hourly_rate: 39,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 home improvement tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[18]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab8}.jpg"),filename:"rab8.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Home Improvement',
+   contact: "tasker19@gmail.com",
+   title: "Im a home improvement helper",
+   hourly_rate: 45,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 home improvement tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[19]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab9}.jpg"),filename:"rab9.jpg")
+
+ Tasker.create!(
+   task_category: 'Furniture Assembly',
+   contact: "tasker20@gmail.com",
+   title: "Im a furniture Assembly helper",
+   hourly_rate: 20,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 assemblytasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[20]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab10}.jpg"),filename:"rab10.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Furniture Assembly',
+   contact: "tasker21@gmail.com",
+   title: "Im a furniture Assembly helper",
+   hourly_rate: 29,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 assemblytasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[21]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab11}.png"),filename:"rab11.png")
+
+
+ Tasker.create!(
+   task_category: 'Furniture Assembly',
+   contact: "tasker22@gmail.com",
+   title: "Im a furniture Assembly helper",
+   hourly_rate: 42,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 assemblytasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[22]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab12}.jpg"),filename:"rab12.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Furniture Assembly',
+   contact: "tasker23@gmail.com",
+   title: "Im a furniture Assembly helper",
+   hourly_rate: 42,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 assemblytasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[23]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab19}.png"),filename:"rab19.png")
+
+
+ Tasker.create!(
+   task_category: 'Moving and Packing',
+   contact: "tasker24@gmail.com",
+   title: "Im a moving & packing helper",
+   hourly_rate: 34,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 moving tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[24]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab13}.jpg"),filename:"rab13.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Moving and Packing',
+   contact: "tasker25@gmail.com",
+   title: "Im a moving & packing helper",
+   hourly_rate: 39,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 moving tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[25]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab14}.jpg"),filename:"rab14.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Moving and Packing',
+   contact: "tasker26@gmail.com",
+   title: "Im a moving & packing helper",
+   hourly_rate: 37,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 moving tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[26]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab15}.jpg"),filename:"rab15.jpg")
+
+ Tasker.create!(
+   task_category: 'Moving and Packing',
+   contact: "tasker27@gmail.com",
+   title: "Im a moving & packing helper",
+   hourly_rate: 43,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 moving tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[27]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab16}.jpg"),filename:"rab16.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Furniture Assembly',
+   contact: "tasker28@gmail.com",
+   title: "Im a furniture Assembly helper",
+   hourly_rate: 42,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 assemblytasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[28]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab17}.jpg"),filename:"rab17.jpg")
+
+
+ Tasker.create!(
+   task_category: 'Heavy Lifting',
+   contact: "tasker29@gmail.com",
+   title: "Im a lifting helper",
+   hourly_rate: 35,
+   city: "San Francisco",
+   description: "I am good at my job.I have completed 100 lifting tasks so far.
+   Experienced Task Rabbit for this job w/ countless professional mounting experiences thru construction projects.
+    A veterian of Takl and Handy Pro platforms.",
+   user: User.all[29]
+ )
+ Tasker.last.photo.attach(io: File.open("#{Rails.root}/app/assets/images/#{rab18}.jpeg"),filename:"rab18.jpeg")
